823e38271a992ac0bcef606c6138bc77cfb30615
spiderdab <77dab77@gmail.com>
2022-11-02 14:27:50 +0100

    added login config variables

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppConfig } from '../services/app.config';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public userIsLogged = false;  // temp variable to show login form
+  public showSignUp = AppConfig.settings.loginSettings.showSignUp;
+  public loginText = AppConfig.settings.loginSettings.text;
+  public showForgotPassword = AppConfig.settings.loginSettings.showForgotPassword;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log("ShowSignUp: " + this.showSignUp);
   }
 
+  onLoginClicked() {
+    this.userIsLogged = true;
+  }
 }
