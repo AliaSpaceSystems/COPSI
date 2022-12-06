1fbd354f2b881039b63f0eb16694aba01b1e3acb
spiderdab <77dab77@gmail.com>
2022-12-06 15:30:19 +0100

    fixed footer covering the product list

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,27 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, transition, style, animate, group, state } from '@angular/animations';
 import { AppConfig } from '../services/app.config';
+
+let footerContainer: any;
+let footerMainDiv: any;
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
-  animations: [
-    trigger('infoAnimation', [
-      state('open', style({
-        'bottom': '1rem', 'opacity': 1
-      })),
-      state('closed', style({
-        'bottom': '0rem', 'opacity': 0
-      })),
-      transition('closed => open', [
-        animate('250ms ease-in')
-      ]),
-      transition('open => closed', [
-        animate('250ms ease-out')
-      ])
-    ])
-  ]
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
 
@@ -33,10 +19,27 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    footerContainer = document.getElementById('footer-container-div')!;
+    footerMainDiv = document.getElementById('footer-main-div')!;
   }
 
   onInfoIconClicked(event: any) {
     this.showInfo = !this.showInfo;
+    if (this.showInfo) {
+      footerContainer.style.left = '0';
+      footerMainDiv.style.display = 'flex';
+      setTimeout(() => {
+        footerMainDiv.style.marginRight = '0';
+        footerMainDiv.style.opacity = '1.0';
+      }, 10);
+    } else {
+      footerMainDiv.style.marginRight = '-0.5rem';
+      footerMainDiv.style.opacity = '0.0';
+      setTimeout(() => {
+        footerContainer.style.left = 'auto';
+        footerMainDiv.style.display = 'none';
+      }, 250);
+    }
     event.stopPropagation();
   }
 }
