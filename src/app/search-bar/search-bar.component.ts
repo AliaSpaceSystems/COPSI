ac548bc4e61537a34564dd376daa061fd11caa54
spiderdab <77dab77@gmail.com>
2026-03-17 12:46:16 +0100

    fixed checkGssProtocols

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -500,12 +500,12 @@ export class SearchBarComponent implements OnInit, OnDestroy, AfterViewInit {
     forkJoin(calls).subscribe({
       next: (res: any) => {
         if (res.odata) {
-          //console.log("res.odata: ", res.odata);
+          console.log("res.odata: ", res.odata);
           if (res.odata.error) {
             console.log("res.odata.error: ", res.odata.error);
             this.isOdataActive = false;
             console.log("OData failed: ", res.odata.details ?? "");
-          } else if (res.odata.data.status == 200 && res.odata.data.body.hasOwnProperty('$Version')) {
+          } else if (res.odata.error == false && res.odata.data.hasOwnProperty('$Version')) {
             this.isOdataActive = true;
           } else {
             this.isOdataActive = false;
